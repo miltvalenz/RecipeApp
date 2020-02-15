f6aaea7a58ca50b18ee714bf85f022a1071ab409
MiltonValenzuela <miltvalenz@hotmail.com>
2020-02-15 18:57:18 -0400

    Receive data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 const App = () => {
   const APP_ID = 'b6840015';
   const APP_KEY = '9114db96a774f8bfa1ec02f93c0c46c3';
+
+  const [recipes, setRepices] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRepices(data.hits);
+    console.log(data.hits);
+  };
 
   return (
     <div className="app">
@@ -11,6 +25,9 @@ const App = () => {
         <input type="text" className="search-bar" />
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe />
+      ))}
     </div>
   );
 }

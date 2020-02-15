1c81da4f451434e8b711cb1392b421d2c686adf4
MiltonValenzuela <miltvalenz@hotmail.com>
2020-02-15 17:33:34 -0400

    Recipe API Keys

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+  const APP_ID = 'b6840015';
+  const APP_KEY = '9114db96a774f8bfa1ec02f93c0c46c3';
+
   return (
     <div className="app">
       <form action="" className="search-form">
